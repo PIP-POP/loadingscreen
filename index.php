f8a491dd430468c5e30aefa9df4eb0cfe1c34f04
PIP-POP <claytonmillward22@gmail.com>
2017-02-24 14:58:01 +0000

    loadingscreen

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	
+		<?php include("inc/php/global.php") ?>
+		<?php include("inc/php/api.php") ?>
+		
+		<title><?php echo $servername ?></title>
+		<link href="inc/css/style.css" rel="stylesheet" type="text/css">
+		<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,900" rel="stylesheet">
+		<script src="https://use.fontawesome.com/c87fd1e779.js"></script>
+		
+	</head>
+	
+	<body>
+		
+		<div class="mid">
+			<a href="./load.php">Loading Screen</a>
+			<a href="./adminlogin.php">Admin Login</a>
+			<a href="./installation.txt" target="_blank">Installation</a>
+		</div>
+		
+		<div class="dark-overlay"></div>
+		
+		<script src="inc/js/jquery.js"></script>
+		
+	</body>
+</html>
